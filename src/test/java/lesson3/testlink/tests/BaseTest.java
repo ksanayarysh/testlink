bb9a6415f17bd8b42af0bc5b092244731f390991
Ksenia Yarysh <ksana32@gmail.com>
2019-06-27 12:04:05 +0500

    browser

--- src/test/java/lesson3/testlink/tests/BaseTest.java
+++ src/test/java/lesson3/testlink/tests/BaseTest.java
@@ -1,14 +1,20 @@
 package lesson3.testlink.tests;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import lesson3.testlink.objects.TestCaseObject;
-import lesson3.testlink.objects.TestPlan;
+import net.lightbody.bmp.core.har.Har;
+import net.lightbody.bmp.proxy.ProxyServer;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.Proxy;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.CapabilityType;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
-import io.github.bonigarcia.wdm.WebDriverManager;
 
 
 public class BaseTest {
@@ -16,6 +22,7 @@ public class BaseTest {
 
     protected final String baseURI = "http://localhost:12325/testlink/";
     protected final String testSuiteName = "TestSuite3";
+    private static ProxyServer server;
     protected TestCaseObject testCase1 = new TestCaseObject();
 
 
@@ -24,17 +31,32 @@ public class BaseTest {
 
 
     @BeforeAll
-    public static void setUp() {
+    public static void setUp() throws Exception {
         WebDriverManager.chromedriver().setup();
 
-        wd = new ChromeDriver();
+        server = new ProxyServer(4444);
+        server.start();
+
+        Proxy proxy = server.seleniumProxy();
+        proxy.setHttpProxy("localhost:4444");
+
+
+        ChromeOptions options = new ChromeOptions();
+        options.setCapability(CapabilityType.PROXY, proxy);
+        wd = new ChromeDriver(options);
+
+        server.newHar("testlink");
+
         wd.manage().window().maximize();
         wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 
     }
 
     @AfterAll
-    public static void tearDown() {
+    public static void tearDown() throws IOException {
+        Har har = server.getHar();
+        har.writeTo(new File( System.getProperty("user.dir") + "/logs.json"));
         wd.quit();
+
     }
 }
